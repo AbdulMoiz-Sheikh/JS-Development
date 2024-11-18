aff8809b00fb03322fc90bdfe6248c43a7738eee
Abdul Moiz <159163812+AbdulMoiz-Sheikh@users.noreply.github.com>
2024-11-18 14:19:24 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"Use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using node js, not browser
+console.log(3 + 3)
+console.log("Abdul Moiz")
+
+let name = "Abdul Moiz"
+let age = 17
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint 
+// string => "name"
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+// object
+
+console.log(typeof undefined)
+
