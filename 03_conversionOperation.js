6d6deacaefd350f8e7d83a6392e9d05ae49db40a
Abdul Moiz <159163812+AbdulMoiz-Sheikh@users.noreply.github.com>
2024-11-20 18:47:34 +0000

    Operations discussed

---
+++ 03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// ******************** Oprations ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "Moiz"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2"+2);
+// console.log(2+2 + "2");
+// console.log(10+2 + "5");
+
+// console.log( (2+2) * "5" % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter--;
+console.log(gameCounter);
